d50b53455f2a7e96651e3ac19fa339c9dc0f1eb2
danielvonboros <dboros40@gmail.com>
2021-09-05 16:29:50 +0200

    add logic to user-login-form

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+// Core modules
+import { Component, OnInit, Input } from '@angular/core';
+
+// custom components
+import { UserLoginService } from '../fetch-api-data.service';
+
+// Material components
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-login-form',
@@ -7,9 +15,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() userData= {username:'', password:''}
+
+  constructor(
+    public fetchApiData: UserLoginService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  // function for sending the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      this.dialogRef.close();
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      })
+    })
   }
 
 }

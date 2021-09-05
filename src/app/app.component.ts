c615722f1210650622d062617bc38ba4026c88dd
danielvonboros <dboros40@gmail.com>
2021-09-05 14:19:49 +0200

    add material to app.component.ts, root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
+// Core modules
 import { Component } from '@angular/core';
+
+// components
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+
+// material components
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filmopedia-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  // the function that will be opened once the signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.openUserRegistrationDialog.open(UserRegistrationFormComponent, {
+      // Assigning a width to the dialog
+      width: '280px'
+    });
+  }
+
 }

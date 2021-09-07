54903077383fc64b8ed56a1c0a057f54f3f8d7e8
danielvonboros <dboros40@gmail.com>
2021-09-07 16:10:53 +0200

    add favorite movies component

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,15 +1,55 @@
+// core modules
 import { Component, OnInit } from '@angular/core';
+
+// custom components
+import { FetchDataApiService } from '../fetch-api-data.service';
+// import MovieGenre / MovieDirector / MovieSynopsis
+
+// material modules
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css']
 })
-export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+const user = localStorage.getItem('username');
+
+export class FavoritesComponent implements OnInit {
+  // isLoading: false;
+  user: any = {};
+  favorites: any = [];
+  movies: any[] = [];
+  favs: any[] = [];
+
+  constructor(
+    public fetchApiData: FetchDataApiService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit(): void {
+    this.getMovies();
+    this.getUsersFavs();
+  }
+
+  getMovies(): void {
+    // this.isLoading: true
+    this.fetchApiData.getAllMovies().subscribe((resp:any) => {
+      // this.isLoading: false
+      this.movies = resp;
+      console.log(this.movies);
+    })
+  }
+
+  getUsersFavs(): void {
+    this.fetchApiData.getUser(user).subscribe((resp:any) => {
+      this.favs = resp.favoritemovies;
+      console.log(this.favs);
+      return this.favs;
+    })
   }
 
 }
